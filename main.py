fefb978c68333e3bd2ffdb32c51d5ab4b41ac35b
LiuYang <ailinal@yahoo.com>
2020-11-25 15:24:32 +0800

    update

---
+++ main.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime
+import zlib
+import pandas as pd
+
+
+# def fuc(n):
+#     ans = 0.0
+#     for i in range(n):
+#         ans += 3 ** i
+#     ans -= (3 ** (n + 1)) / 2
+#     print(ans)
+#     return ans
+#
+#
+# x = [x for x in range(300)]
+# y = [fuc(x) for x in range(300)]
+# plt.scatter(x, y)
+#
+# plt.show()
+
+
+z=1
+for i in map(int,z):
+    print(i)
