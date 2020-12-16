b7a9435a12cf0050cd392d1dda48d10b5f9728c0
ailinal <ailinal@yahoo.com>
2020-12-16 21:13:05 +0800

    ch7

--- main.py
+++ main.py
@@ -3,22 +3,5 @@ import numpy as np
 import datetime
 import zlib
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
-
-# def fuc(n):
-#     ans = 0.0
-#     for i in range(n):
-#         ans += 3 ** i
-#     ans -= (3 ** (n + 1)) / 2
-#     print(ans)
-#     return ans
-#
-#
-# x = [x for x in range(300)]
-# y = [fuc(x) for x in range(300)]
-# plt.scatter(x, y)
-#
-# plt.show()
-
-
-plt.plot()
